a56052d9d47c69fdc4b2e7159fed4b1694d8ce7e
Rashedul Alam <rashed464@outlook.com>
2025-11-02 00:14:05 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -14,9 +15,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          {/* 404 Not Found */}
-          <Route path="*" element={<h2>404 Page Not Found</h2>} />
+          <Route path="contact" element={<Contact />} />         
+         <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
